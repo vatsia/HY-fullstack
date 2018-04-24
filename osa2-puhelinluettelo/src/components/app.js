713191aa35cd1c4af840a68f5ee3808e8c0aef7e
Olli Vatsia <olli.vatsia@gmail.com>
2018-04-24 21:14:34 +0300

    tehtävä 2.8

--- osa2-puhelinluettelo/src/components/app.js
+++ osa2-puhelinluettelo/src/components/app.js
@@ -5,23 +5,27 @@ class App extends React.Component {
     super(props)
     this.state = {
       persons: [
-        { name: 'Arto Hellas' }
+        { name: 'Arto Hellas', number: '050-1234123' }
       ],
-      newName: ''
+      newName: '',
+      newNumber: ''
     }
   }
 
   addNumber = (event) => {
     event.preventDefault()
+
     const numberObject = {
-            name: this.state.newName
+            name: this.state.newName,
+            number: this.state.newNumber
     }
-    console.log(this.state.persons.indexOf(numberObject) )
+
     if(!this.state.persons.some(ps => ps.name === numberObject.name)){
         const numbers = this.state.persons.concat(numberObject);
         this.setState({
             persons: numbers,
-            newName: ''
+            newName: '',
+            newNumber: ''
         });
     }else{
         alert("Numero on jo lisätty!");
@@ -29,8 +33,12 @@ class App extends React.Component {
 
   }
 
-  handleNumberChange = (event) => {
+  handleNameChange = (event) => {
     this.setState({newName: event.target.value});
+  }
+
+  handleNumberChange = (event) => {
+    this.setState({newNumber: event.target.value});
   }
 
   render() {
@@ -39,16 +47,21 @@ class App extends React.Component {
         <h2>Puhelinluettelo</h2>
         <form onSubmit={this.addNumber}>
           <div>
-            nimi: <input value={this.state.newName} onChange={this.handleNumberChange} />
+            nimi: <input value={this.state.newName} onChange={this.handleNameChange} />
+          </div>
+          <div>
+            numero: <input value={this.state.newNumber} onChange={this.handleNumberChange} />
           </div>
           <div>
             <button type="submit">lisää</button>
           </div>
         </form>
         <h2>Numerot</h2>
-        <ul>
-            {this.state.persons.map(person => <li key={person.name}>{person.name}</li>)}
-        </ul>
+        <table>
+            <tbody>
+                {this.state.persons.map(person => <tr key={person.name}><td>{person.name}</td><td>{person.number}</td></tr>)}
+            </tbody>
+        </table>
       </div>
     )
   }
